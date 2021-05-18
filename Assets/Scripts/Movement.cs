f1ef2fd6e4573aa6e47281ae42822e6d9a967681
Liam Shearan <liamshearan@gmail.com>
2021-05-18 11:29:46 +0100

    Inital Commit of Project
    Basic Control of the Rocket
    Basic World built aswell

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    [SerializeField] private float _thrustPower = 100f;
+    [SerializeField] private float _rotationRate = 100f;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Process Inputs from the Player
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        // Apply The Force To the Rocket if the SPACE Key Is Pressed
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddRelativeForce(Time.deltaTime * _thrustPower * Vector3.up);
+        }
+
+    }
+
+    private void ProcessRotation()
+    {
+        // Rotate the Rocket Left or Right Depending On the Key Pressed (A or D)
+        // Only Allow One Of the Keys to Do Something So the Player Cant Rotate the Rocket
+        // Both Left & Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-_rotationRate);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(_rotationRate);
+        }
+
+    }
+
+    private void ApplyRotation(float rotation)
+    {
+        // Freezing rotation so we can manually rotate
+        _rigidbody.freezeRotation = true;
+        
+        transform.Rotate(Time.deltaTime * rotation * Vector3.forward);
+        
+        //Unfreezing rotation so the physics system can take over and handle collision
+        _rigidbody.freezeRotation = false;
+    }
+    
+}
